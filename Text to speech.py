41bb2debe7944f92c3906e318547bfa1670c9de1
sanketparajuli <parajulisanket7@gmail.com>
2023-12-30 19:55:03 +0545

    modified

--- Text to speech.py
+++ Text to speech.py
@@ -1,11 +1,12 @@
-while True:
-    import gtts
-    import playsound
+# while True:
+#     import gtts
+#     import playsound
 
-    user = input("enter the text: ") # takes the text from the user.
-    sound = gtts.gTTS(user, tld="co.in")
+#     user = input("enter the text: ") # takes the text from the user.
+#     sound = gtts.gTTS(user, tld="co.in")
 
-    sound.save("audio.mp3") # Saves the audio in mp3.
-    playsound.playsound("audio.mp3") # Plays the audio of the text that was save.
+#     sound.save("audio.mp3") # Saves the audio in mp3.
+#     playsound.playsound("audio.mp3") # Plays the audio of the text that was save.
+
 
 
